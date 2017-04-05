515fcbcb093c08286f525b9425b042ce2cd5a8af
Sideus <kyal.smith@gmail.com>
2017-04-05 17:51:17 +0200

    Maze Script

---
+++ Downloads/Scripts/Maze_Navigation.py
@@ -0,0 +1,228 @@
+
+output_Sting=''
+found = False
+def checkio(maze_map):
+    global output_Sting
+    global found
+
+    output_Sting = ''
+    found = False
+    starting_position = (1, 1)
+
+    previous_position = [999, 999]
+
+    routes = [Route(starting_position, previous_position, "")]
+
+    while not found:
+        Run(routes, maze_map)
+    print(output_Sting+"\n")
+
+
+    return output_Sting
+
+
+def Run(routes, maze_map):
+
+
+    global found
+
+    for r in routes:
+        for count, inner_r in enumerate(routes):
+            if inner_r.children == True or inner_r.dead_end == True:
+                del routes[count]
+
+        check_position(r,routes, maze_map)
+        if found:
+            break
+    if found:
+        return
+
+
+
+
+
+
+
+
+
+
+def check_position(Route1,routes, maze_map):
+    global found
+    global output_Sting
+    route_count = 0
+    new_coords = {}
+    current = Route1.current_position
+    if current == [10,10]:
+        return found
+
+    test1 = maze_map[current[1]][current[0] - 1]
+    test2 = maze_map[current[1]][current[0] + 1]
+    test3 = maze_map[current[1] + 1][current[0]]
+    test4 = maze_map[current[1] - 1][current[0]]
+
+    if test1 == 0 and [current[0] - 1, current[1]] != Route1.previous_position:
+        route_count += 1
+        new_coords["W"] = [current[0] - 1, current[1]]
+    if test2 == 0 and [current[0] + 11, current[1]] != Route1.previous_position:
+        route_count += 1
+        new_coords["E"] = [current[0] + 1, current[1]]
+    if test3 == 0 and [current[0], current[1] + 1] != Route1.previous_position:
+        route_count += 1
+        new_coords["S"] = [current[0], current[1] + 1]
+    if test4 == 0 and [current[0], current[1] - 1] != Route1.previous_position:
+        route_count += 1
+        new_coords["N"] = [current[0], current[1] - 1]
+
+    if route_count < 1:
+        Route1.dead_end = True
+
+    if route_count > 1 and len(new_coords) > 1:
+        for val in new_coords:
+
+            routes.append(Route(new_coords[val], current, Route1.route+val))
+            Route1.children = True
+            if new_coords[val] == [10, 10]:
+                output_Sting = Route1.route+val
+                found = True
+                return
+            
+
+
+
+    elif route_count == 1:
+        found = Move(Route1, new_coords)
+        if found:
+            return
+
+
+
+def Move(route_obj, coords):
+    global output_Sting
+    route_obj.previous_position = route_obj.current_position
+    for item in coords:
+        route_obj.current_position = list(coords[item])
+        route_obj.route += item
+        route_obj.route_coords.append(coords)
+    if coords[item] == [10, 10]:
+        output_Sting = str(route_obj.route)
+        return True
+
+
+class Route(object):
+    def __init__(self, starting_position, previous_position, route):
+        self.connect_locations = []
+        self.children = False
+        self.dead_end = False
+
+        self.route_coords = []
+        self.start_position = starting_position
+        self.route = route
+        self.current_position = list(self.start_position)
+        self.route_coords.append(self.current_position)
+        self.previous_position = previous_position
+
+
+if __name__ == '__main__':
+    # This code using only for self-checking and not necessary for auto-testing
+    def check_route(func, labyrinth):
+        MOVE = {"S": (1, 0), "N": (-1, 0), "W": (0, -1), "E": (0, 1)}
+        # copy maze
+        route = func([row[:] for row in labyrinth])
+        pos = (1, 1)
+        goal = (10, 10)
+        for i, d in enumerate(route):
+            move = MOVE.get(d, None)
+            if not move:
+                print("Wrong symbol in route")
+                return False
+            pos = pos[0] + move[0], pos[1] + move[1]
+            if pos == goal:
+                return True
+            if labyrinth[pos[0]][pos[1]] == 1:
+                print("Player in the pit")
+                return False
+        print("Player did not reach exit")
+        return False
+
+
+    # These assert are using only for self-testing as examples.
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+        [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "First maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Empty maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Up and down maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Dotted maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Need left maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "The big dead end."
+    print("The local tests are done.")
+
